dba600f5e7b5871d5df82d9709733146dbfbebdc
maltamash <moh-alt@hotmail.co.uk>
2018-11-19 11:27:06 +0000

    Playing with terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region = "eu-west-1"
-}
-
 resource "aws_instance" "terraform-instance" {
-  ami = "ami-0e0739088d27ce0fc"
-  instance_type = "t2.micro"
+  ami = "${var.ami}"
+  instance_type = "t3.large"
 }
